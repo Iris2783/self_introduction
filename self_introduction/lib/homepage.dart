3d9df9404b954df5652acf17727408fc43daf128
Iris2783 <iris.corp.2783@gmail.com>
2023-02-23 06:13:36 +0900

    edit

--- self_introduction/lib/homepage.dart
+++ self_introduction/lib/homepage.dart
@@ -9,6 +9,88 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('My Home Page'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 130),
+        child: Center(
+          child: Column(
+            children: [
+              const CircleAvatar(
+                backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqEm_MZAbuNH9Kv152U3l5Q2YBMhSsk7CGpQ&usqp=CAU'),
+                radius: 100,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    'Name: ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  Text(
+                    'Jack Hair',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.only(left: 110),
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Age:',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      '30',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.only(left: 110),
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Original:',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      'Japan',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
